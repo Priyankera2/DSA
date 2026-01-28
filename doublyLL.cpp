43a88fa2fa985c91121b0e695eef154fbb319787
Priyanka <priyankera2@gmail.com>
2026-01-28 14:32:01 +0530

    delete operations on doubly linked list updated

--- doublyLL.cpp
+++ doublyLL.cpp
@@ -37,6 +37,80 @@ Node* deletetail(Node* head){
      delete(tail);
      return head;
 }
+Node* deletekthNode(Node* head,int k){
+    int cnt=0;
+    
+    Node* temp=head;
+    while(temp){
+        cnt++;
+        if(cnt==k)break;temp=temp->next;
+    }
+    if(k>cnt||k<1)return head;
+    Node* back=temp->prev;
+    Node* front=temp->next;
+
+    if(back==nullptr&&front==nullptr){                 // edge case1: single element case , then prev and next will be null
+        delete(head);
+        return NULL;}
+
+    else if(back==NULL){                              // edge case2: k=1 , then temp is head and prev of head is null;
+        front->prev=nullptr;
+        delete(temp);
+        return front;
+    }
+    else if(front==nullptr){                           // edge case3: kth node is tail, then front will be null;
+        back->next=nullptr;
+        delete(temp);
+        return head;
+    }
+    back->next=front;                                 // middle node deletion
+    front->prev=back;
+    temp->prev=nullptr;
+    temp->next=nullptr;
+    delete(temp);
+    return head;
+    
+}
+Node* deleteNodeValue(Node* head, int node){
+    Node* temp =head;
+   
+    while(temp&&temp->data!=node){
+        temp=temp->next;
+    }
+    if(!temp)return head;
+    Node* back=temp->prev;
+    Node* front=temp->next;
+    if(front==nullptr){
+        back->next=nullptr;
+        temp->prev=nullptr;
+        delete(temp);
+        return head;
+
+    }
+    front->prev=back;
+    back->next=front;
+    temp->prev=nullptr;
+    temp->next=nullptr;
+    delete(temp);
+    return head;
+
+}
+void deleteNode(Node* node){
+    Node* back=node->prev,*front=node->next;
+    if(front==nullptr){
+        back->next=nullptr;
+        node->prev=nullptr;
+        delete(node);
+
+    }
+   
+    else{
+    back->next=front;
+    front->prev=back;
+    node->next=nullptr;
+    node->prev=nullptr;
+    delete(node);}
+}
 int main(){
      int  arr[]={1,2,3,4};
      Node* head=new Node(arr[0]);
@@ -47,9 +121,11 @@ int main(){
         prev->next=temp;
         prev=temp;
      }
-     deletetail(head);
-     while(head){
-        cout<<head->data<<" ";
-        head=head->next;
+     Node* temp=head;
+     deleteNode(head->next->next);
+     
+     while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
      }
 }
